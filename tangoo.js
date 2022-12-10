143216dd7942caf60ec9febfd7856aaed097884e
h.matsuo <matsuoshi@gmail.com>
2022-12-10 19:53:11 +0900

    randomize

--- tangoo.js
+++ tangoo.js
@@ -1,13 +1,15 @@
 import * as fs from 'node:fs'
 import { parse } from 'csv-parse/sync'
+import arrayShuffle from 'array-shuffle'
 import readline from 'node:readline/promises'
 import { stdin as input, stdout as output } from 'node:process'
 
 function readCsv(filename)
 {
-  const csv = fs.readFileSync('words.csv', 'utf8')
-  return parse(csv, {columns: true})
+  const csv = fs.readFileSync(filename, 'utf8')
+  const words = parse(csv, {columns: true})
     .filter(word => word.complete !== '1')
+  return arrayShuffle(words)
 }
 
 async function question(words)
@@ -22,5 +24,5 @@ async function question(words)
   rl.close()
 }
 
-const words = readCsv()
+const words = readCsv('words.csv')
 question(words)

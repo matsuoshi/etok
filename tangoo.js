26ef2f65621fe12c7866366b34e71806f1fa3f5c
h.matsuo <matsuoshi@gmail.com>
2022-12-10 19:59:54 +0900

    code format

--- tangoo.js
+++ tangoo.js
@@ -1,28 +1,28 @@
-import * as fs from 'node:fs'
+import fs from 'node:fs'
 import { parse } from 'csv-parse/sync'
 import arrayShuffle from 'array-shuffle'
 import readline from 'node:readline/promises'
 import { stdin as input, stdout as output } from 'node:process'
 
-function readCsv(filename)
-{
+function readCsv(filename) {
   const csv = fs.readFileSync(filename, 'utf8')
-  const words = parse(csv, {columns: true})
+  const words = parse(csv, { columns: true })
     .filter(word => word.complete !== '1')
   return arrayShuffle(words)
 }
 
-async function question(words)
-{
-  const rl = readline.createInterface({input, output})
+async function question(words) {
+  const rl = readline.createInterface({ input, output })
 
-  for (let i in words) {
-    const string = await rl.question(`${words[i].en} ? `)
-    console.log(words[i].ja)
+  for (const word of words) {
+    const string = await rl.question(`${word.en} ? `)
+    console.log(word.ja)
   }
 
   rl.close()
 }
 
-const words = readCsv('words.csv')
-question(words)
+(function main() {
+  const words = readCsv('words.csv')
+  question(words)
+})()
